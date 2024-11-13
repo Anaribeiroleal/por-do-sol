a172cb9e00a2c289ddd7e0d69b53b1b8f0749cd0
Anaribeiroleal <ana.ribeiro.leal@escola.pr.gov.br>
2024-11-13 16:44:39 -0300

    13

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let posicaoVertical;
+
+function setup() {
+    createCanvas(500, 500);
+    createCanvas(500, 500);
+    posicaoVertical = 250;
+}
+
+function draw() {
+    if (posicaoVertical < 400) {
+        background ("lighblue");
+
+
